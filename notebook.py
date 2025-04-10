38b8ad2f38f790c554c3c87464733d2ad8fe417a
tiagojbandrade <tiagobarbosajp@yahoo.com.br>
2025-04-09 22:10:53 -0300

    terceiro commit

--- notebook.py
+++ notebook.py
@@ -45,8 +45,63 @@ df = df.dropna(how='all')
 # Notice we use df and not df_view
 # With DuckDB you can run SQL queries on top of Pandas dataframes
 conn.execute("SELECT COUNT(*) FROM df").df()
+
 # %%
 
 #Where clause
 conn.execute("""SELECT * FROM df WHERE "Order ID"='295665'""").df()
+
+# %%
+# Limpa a vírgula da coluna "Price"
+df["Price"] = df["Price"].str.replace(',', '').astype(float)
+# %%
+#Create a table and load the data
+conn.execute("""
+CREATE OR REPLACE TABLE sales AS
+	SELECT
+		"Order ID"::INTEGER AS order_id,
+		Product AS product,
+		"Quantity Ordered"::INTEGER AS quantity,
+		"Price"::DECIMAL AS price,
+		"Order Date"::DATE AS order_date,
+		"Purchase Address" AS purchase_address
+	FROM df
+	WHERE
+		TRY_CAST("Order ID" AS INTEGER) NOTNULL
+""")
+
+# %%
+#FROM-first clause
+conn.execute("FROM sales").df()
+
+# %%
+#Exclude
+conn.execute("""
+	SELECT 
+		* EXCLUDE (product, order_date, purchase_address)
+	FROM sales
+	""").df()
+
+# %%
+#The Columns Expression
+conn.execute("""
+	SELECT 
+		MIN(COLUMNS(* EXCLUDE (product, order_date, purchase_address))) 
+	FROM sales
+	""").df()
+
+# %%
+#Create a VIEW
+conn.execute("""
+	CREATE OR REPLACE VIEW aggregated_sales AS
+	SELECT
+		order_id,
+		COUNT(1) as nb_orders,
+		MONTH(order_date) as month,
+		str_split(purchase_address, ',')[2] AS city,
+		SUM(quantity * price) AS revenue
+	FROM sales
+	GROUP BY ALL
+""")
+
 # %%
